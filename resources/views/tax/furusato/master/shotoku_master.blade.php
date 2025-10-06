f01cd80c4931088f31c6e74faf24e71ce36bb507
masashishikatani17 <masashi@stgate.co.jp>
2025-10-06 13:23:10 +0900

    マスター作成

--- resources/views/tax/furusato/master/shotoku_master.blade.php
+++ resources/views/tax/furusato/master/shotoku_master.blade.php
@@ -1,10 +1,68 @@
 @extends('layouts.min')
-
 @section('content')
 <div class="container" style="min-width: 960px; max-width: 1080px;">
   <div class="d-flex justify-content-between align-items-center mb-3">
     <h5 class="mb-0">ふるさと納税：所得税率マスター</h5>
     <a href="{{ route('furusato.master', ['data_id' => $dataId]) }}" class="btn btn-outline-secondary btn-sm">戻る</a>
   </div>
+
+  <table class="table table-sm table-bordered align-middle">
+    <tbody>
+      <tr>
+        <th colspan="3">課税される所得金額</th>
+        <th class="text-end">税率</th>
+        <th class="text-end">控除額</th>
+      </tr>
+      <tr>
+        <td class="text-end">0</td>
+        <td class="text-center">～</td>
+        <td class="text-end">1,949,000</td>
+        <td class="text-end">5%</td>
+        <td class="text-end">0</td>
+      </tr>
+      <tr>
+        <td class="text-end">1,950,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">3,299,000</td>
+        <td class="text-end">10%</td>
+        <td class="text-end">97,500</td>
+      </tr>
+      <tr>
+        <td class="text-end">3,300,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">6,949,000</td>
+        <td class="text-end">20%</td>
+        <td class="text-end">427,500</td>
+      </tr>
+      <tr>
+        <td class="text-end">6,950,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">8,999,000</td>
+        <td class="text-end">23%</td>
+        <td class="text-end">636,000</td>
+      </tr>
+      <tr>
+        <td class="text-end">9,000,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">17,999,000</td>
+        <td class="text-end">33%</td>
+        <td class="text-end">1,536,000</td>
+      </tr>
+      <tr>
+        <td class="text-end">18,000,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">39,999,000</td>
+        <td class="text-end">40%</td>
+        <td class="text-end">2,796,000</td>
+      </tr>
+      <tr>
+        <td class="text-end">40,000,000</td>
+        <td class="text-center">～</td>
+        <td></td>
+        <td class="text-end">45%</td>
+        <td class="text-end">4,796,000</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 @endsection
--- resources/views/tax/furusato/master/tokurei_master.blade.php
+++ resources/views/tax/furusato/master/tokurei_master.blade.php
@@ -1,10 +1,49 @@
 @extends('layouts.min')
-
 @section('content')
 <div class="container" style="min-width: 960px; max-width: 1080px;">
   <div class="d-flex justify-content-between align-items-center mb-3">
     <h5 class="mb-0">ふるさと納税：特例控除マスター</h5>
     <a href="{{ route('furusato.master', ['data_id' => $dataId]) }}" class="btn btn-outline-secondary btn-sm">戻る</a>
   </div>
+  <table class="table table-sm table-bordered align-middle">
+    <thead class="table-light">
+      <tr>
+        <th class="text-center" colspan="3">課税所得金額から人的控除差調整額を控除した金額</th>
+        <th class="text-center">申告特例控除の割合</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-end">0</td>
+        <td class="text-center">～</td>
+        <td class="text-end">1,950,000</td>
+        <td class="text-end">5.105/84.895</td>
+      </tr>
+      <tr>
+        <td class="text-end">1,951,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">3,300,000</td>
+        <td class="text-end">10.21/79.79</td>
+      </tr>
+      <tr>
+        <td class="text-end">3,301,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">6,950,000</td>
+        <td class="text-end">20.42/69.58</td>
+      </tr>
+      <tr>
+        <td class="text-end">6,951,000</td>
+        <td class="text-center">～</td>
+        <td class="text-end">9,000,000</td>
+        <td class="text-end">23.483/66.517</td>
+      </tr>
+      <tr>
+        <td class="text-end">9,001,000</td>
+        <td class="text-center">～</td>
+        <td></td>
+        <td class="text-end">33.693/56.307</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 @endsection
